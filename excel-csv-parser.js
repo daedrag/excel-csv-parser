c77eef97317f8a4901d123a61b4b4552a16891e2
daedrag <daedrag@users.noreply.github.com>
2015-12-10 18:05:51 +0800

    Update excel-csv-parser.js
    Remove console log and comments.

--- excel-csv-parser.js
+++ excel-csv-parser.js
@@ -5,16 +5,6 @@
 
   var ExcelCSV = {
 
-    // STATE: {
-    //   // Start of new entry
-    //   NEWLINE: 0,
-    //   // Parse logic in single line, delimited by comma
-    //   SINGLE: 1,
-    //   // Single item spanning on multiple line, expectedly delimited by 2 characters comma and double quote
-    //   // if
-    //   MULTILINE: 2
-    // },
-
     _initIteratee: function (data) {
       return {
         data: data,
@@ -34,7 +24,6 @@
           if (err) {
             reject(err);
           } else {
-            // console.log(data);
             resolve(ExcelCSV._parse(data).out);
           }
         });
@@ -50,7 +39,6 @@
     },
 
     _eventNewline: function (iteratee) {
-      // console.log('In EventNewLine: ');
       // this is transition state only
       if (iteratee.data.length == 0) return;
 
@@ -68,14 +56,10 @@
     },
 
     _eventSingle: function (iteratee) {
-      // console.log('In EventSingle: ');
       // enter this state meaning that the multiline flag is not set
       var startIndex = _.indexOf(iteratee.data, ',');
 
-      // // console.log('    > index: ' + startIndex);
-      // // console.log('    > rows: ' + iteratee.rows);
       if (startIndex === -1 || startIndex === iteratee.data.length) {
-        // console.log('    > this is end of data');
         // if this is the end of the data
         // conclude everything, ignore last character
         if (startIndex === -1) {
@@ -87,23 +71,13 @@
         iteratee.data = '';
         // iteratee.multilineFlag = false;
       } else if (iteratee.data[startIndex + 1] === '"') {
-        // console.log('    > next cell is multiline');
         // next cell is multiline
         iteratee.rows.push(_.first(iteratee.data, startIndex).join(''));
         iteratee.data = _.rest(iteratee.data, startIndex + 2);
         // set multiline flag
         iteratee.multilineFlag = true;
         ExcelCSV._eventMultiline(iteratee);
-      // } else if (iteratee.data[startIndex + 1] === '\n') {
-      //   // console.log('    > this is last cell in the line');
-      //   // this is last cell in the line
-      //   iteratee.rows.push(_.first(iteratee.data, startIndex).join(''));
-      //   iteratee.out.push(iteratee.rows);
-      //   iteratee.rows = [];
-      //   iteratee.data = _.rest(iteratee.data, startIndex + 2);
-      //   ExcelCSV._eventNewline(iteratee);
       } else if (iteratee.data[startIndex + 1] === '\r') {
-        // console.log('    > this is last cell in the line');
         iteratee.rows.push(_.first(iteratee.data, startIndex).join(''));
         // push 1 last empty cell
         iteratee.rows.push('');
@@ -115,8 +89,6 @@
         var newlineIndex = _.indexOf(iteratee.data, '\r');
 
         if (newlineIndex !== -1 && newlineIndex < startIndex) {
-          // console.log('    > this cell is last cell in row');
-          // console.log('    > adding: ' + _.first(iteratee.data, newlineIndex).join(''));
           // next cell is still single cell
           iteratee.rows.push(_.first(iteratee.data, newlineIndex).join(''));
           iteratee.data = _.rest(iteratee.data, newlineIndex + 2);
@@ -124,8 +96,6 @@
           iteratee.rows = [];
           ExcelCSV._eventNewline(iteratee);
         } else {
-          // console.log('    > next cell is single cell');
-          // console.log('    > adding: ' + _.first(iteratee.data, startIndex).join(''));
           // next cell is still single cell
           iteratee.rows.push(_.first(iteratee.data, startIndex).join(''));
           iteratee.data = _.rest(iteratee.data, startIndex + 1);
@@ -135,14 +105,10 @@
     },
 
     _eventMultiline: function (iteratee) {
-      // console.log('In EventMultiLine: ');
       // enter this state meaning that the multiline flag is set
       var endMultilineIndex = _.indexOf(iteratee.data, '"');
 
-      // // console.log('    > index: ' + endMultilineIndex);
-      // // console.log('    > rows: ' + iteratee.rows);
       if (endMultilineIndex === -1 || endMultilineIndex === iteratee.data.length) {
-        // console.log('    > this is end of data');
         // if this is the end of the data
         // conclude everything
         if (endMultilineIndex === -1) {
@@ -152,18 +118,7 @@
         iteratee.out.push(iteratee.rows);
         iteratee.rows = [];
         iteratee.data = '';
-        // iteratee.multilineFlag = false;
-      // } else if (iteratee.data[endMultilineIndex + 1] === '\n') {
-      //   // console.log('    > current row is still belonging to same multiline cell');
-      //   // current row is still belonging to same multiline cell
-      //   // just cut this line and insert to rows array
-      //   iteratee.rows.push(_.first(iteratee.data, endMultilineIndex).join(''));
-      //   iteratee.data = _.rest(iteratee.data, endMultilineIndex + 2);
-      //   iteratee.multilineFlag = false;
-      //   // redirect to new line
-      //   ExcelCSV._eventNewline(iteratee);
       } else if (iteratee.data[endMultilineIndex + 1] === '\r') {
-        // console.log('    > this multiline cell is last in the row');
         iteratee.rows.push(_.first(iteratee.data, endMultilineIndex).join(''));
         // push 1 last empty cell
         iteratee.rows.push('');
@@ -174,7 +129,6 @@
         // redirect to new line
         ExcelCSV._eventNewline(iteratee);
       } else if (iteratee.data[endMultilineIndex + 1] === ',') {
-        // console.log('    > end of multiline cell');
         iteratee.rows.push(_.first(iteratee.data, endMultilineIndex).join(''));
         iteratee.data = _.rest(iteratee.data, endMultilineIndex + 2);
         iteratee.multilineFlag = false;
@@ -189,7 +143,6 @@
           iteratee.data = _.rest(iteratee.data, newlineIndex + 2);
           ExcelCSV._eventMultiline(iteratee);
         } else {
-          // console.log('   > seems multiline cell is closed quote');
           iteratee.rows.push(_.first(iteratee.data, endMultilineIndex).join(''));
           iteratee.data = _.rest(iteratee.data, newlineIndex + 2);
           iteratee.multilineFlag = false;
